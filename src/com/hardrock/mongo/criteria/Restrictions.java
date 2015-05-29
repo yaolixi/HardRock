18fd92fc1f7fa7b3de26eace4e1c618f3c253470
lixi_yao <22617642@qq.com>
2015-05-29 22:19:30 +0800

    implement $ne and $nin

--- src/com/hardrock/mongo/criteria/Restrictions.java
+++ src/com/hardrock/mongo/criteria/Restrictions.java
@@ -6,49 +6,28 @@ import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 
 public class Restrictions {
-	public static Criterion or(Criterion ...args){
-		Criterion criterion = new Criterion();
-		BasicDBList dl = new BasicDBList();
-		for (int i = 0; i < args.length; i++) {
-			dl.add(args[i].getBo());
-		}
-		criterion.getBo().append("$or", dl);
-		return criterion;
-	}
-	
-	public static Criterion and(Criterion ...args){
-		Criterion criterion = new Criterion();
-		BasicDBList dl = new BasicDBList();
-		for (int i = 0; i < args.length; i++) {
-			dl.add(args[i].getBo());
-		}
-		criterion.getBo().append("$and", dl);
-		return criterion;
-	}
+	/**
+	 * Comparison Selectors
+	 * $eq 	Matches values that are equal to a specified value.
+	 * $gt 	Matches values that are greater than a specified value.
+	 * $gte Matches values that are greater than or equal to a specified value.
+	 * $lt 	Matches values that are less than a specified value.
+	 * $lte Matches values that are less than or equal to a specified value.
+	 * $ne 	Matches all values that are not equal to a specified value.
+	 * $in 	Matches any of the values specified in an array.
+	 * $nin Matches none of the values specified in an array.
+	 */
 	
 	public static Criterion eq(String property, Object obj){
 		Criterion criterion = new Criterion();
 		criterion.getBo().append(property, obj);
+//		above is the same as criterion.getBo().append(property, new BasicDBObject("$eq", obj));
 		return criterion;
 	}
 	
-	public static Criterion in(String property, Object[] objs){
+	public static Criterion ne(String property, Object obj){
 		Criterion criterion = new Criterion();
-		BasicDBList dl = new BasicDBList();
-		for(Object obj : objs){
-			dl.add(obj);
-		}
-		criterion.getBo().append(property, new BasicDBObject("$in", dl));
-		return criterion;
-	}
-	
-	public static Criterion in(String property, Collection<? extends Object> objs){
-		Criterion criterion = new Criterion();
-		BasicDBList dl = new BasicDBList();
-		for(Object obj : objs){
-			dl.add(obj);
-		}
-		criterion.getBo().append(property, new BasicDBObject("$in", dl));
+		criterion.getBo().append(property, new BasicDBObject("$ne", obj));
 		return criterion;
 	}
 	
@@ -75,6 +54,82 @@ public class Restrictions {
 		criterion.getBo().append(property, new BasicDBObject("$gte", obj));
 		return criterion;
 	}
+	
+	public static Criterion in(String property, Object[] objs){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for(Object obj : objs){
+			dl.add(obj);
+		}
+		criterion.getBo().append(property, new BasicDBObject("$in", dl));
+		return criterion;
+	}
+	
+	public static Criterion in(String property, Collection<? extends Object> objs){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for(Object obj : objs){
+			dl.add(obj);
+		}
+		criterion.getBo().append(property, new BasicDBObject("$in", dl));
+		return criterion;
+	}
+	
+	public static Criterion nin(String property, Object[] objs){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for(Object obj : objs){
+			dl.add(obj);
+		}
+		criterion.getBo().append(property, new BasicDBObject("$nin", dl));
+		return criterion;
+	}
+	
+	public static Criterion nin(String property, Collection<? extends Object> objs){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for(Object obj : objs){
+			dl.add(obj);
+		}
+		criterion.getBo().append(property, new BasicDBObject("$nin", dl));
+		return criterion;
+	}
+	
+	/**
+	 *  Logical Selectors
+	 *  $or 	Joins query clauses with a logical OR returns all documents that match the conditions of either clause.
+	 *  $and 	Joins query clauses with a logical AND returns all documents that match the conditions of both clauses.
+	 *  $not 	Inverts the effect of a query expression and returns documents that do not match the query expression.
+	 *  $nor 	Joins query clauses with a logical NOR returns all documents that fail to match both clauses.
+	 */
+	
+	public static Criterion or(Criterion ...args){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for (int i = 0; i < args.length; i++) {
+			dl.add(args[i].getBo());
+		}
+		criterion.getBo().append("$or", dl);
+		return criterion;
+	}
+	
+	public static Criterion and(Criterion ...args){
+		Criterion criterion = new Criterion();
+		BasicDBList dl = new BasicDBList();
+		for (int i = 0; i < args.length; i++) {
+			dl.add(args[i].getBo());
+		}
+		criterion.getBo().append("$and", dl);
+		return criterion;
+	}
+	
+	/**
+	 * Evaluation Selectors
+	 * $mod 	Performs a modulo operation on the value of a field and selects documents with a specified result.
+	 * $regex 	Selects documents where values match a specified regular expression.
+	 * $text 	Performs text search.
+	 * $where 	Matches documents that satisfy a JavaScript expression. 
+	 */
 	
 	public static Criterion mod(String property, int divisor, int remainder){
 		Criterion criterion = new Criterion();
